947cb4cb279ac8477019313394672ea93434edbd
Bharat Soni <bharatchills@gmail.com>
2023-02-24 22:25:17 +0530

    centered something good night

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const inputKey = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+      console.log("Dvd")
+      inputKey.current?.focus();
+    }, [])
+    
 
   return (
-    <>
-      check
-    </>
+    <div onPaste={(e)=>console.log('TODO: ON PASE FEATURE IN PROCESS')}  className="flex flex-col items-center justify-center main h-screen w-screen">
+   
+        <div className="flex items-center justify-center ">
+            <input
+            ref={inputKey}
+              className=""
+              id="imagesUpload"
+            />
+        </div>
+    </div>
   )
 }
